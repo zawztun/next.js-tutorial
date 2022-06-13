836403db8fee197694cb71d88f19f38595dea214
zaw tun <zzt.nyc@gmail.com>
2022-06-13 18:31:49 -0400

    add_togglemode

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,15 @@
 import Footer from "./Footer"
 import Navbar from "./Navbar"
+import { useState } from "react";
 
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
+    const [darkMode, setDarkMode] = useState(false);
     return ( 
-        <div className = "content">
-            <Navbar/>
+        <div className = "content" id = {`${darkMode && "dark-mode"}`} >
+            <Navbar
+                darkMode={darkMode}
+                setDarkMode = {setDarkMode}
+            />
             {children}
             <Footer/>
         </div>
--- components/Navbar.js
+++ components/Navbar.js
@@ -2,7 +2,10 @@
 /* eslint-disable jsx-a11y/alt-text */
 import Link  from "next/link";
 import Image from "next/image";
-const Navbar = () => {
+
+
+const Navbar = ({darkMode,setDarkMode}) => {
+    
     return ( 
     <nav>
         <div className = "logo">
@@ -15,9 +18,12 @@ const Navbar = () => {
         </div>
         <Link href = "/"><a>Home</a></Link>
         <Link href = "/about"><a>About</a></Link>
-        <Link href = "/zawtun"><a>Zawtun List</a></Link>  
+            <Link href="/zawtun"><a>Zawtun List</a></Link>  
+            <button className="dark" onClick = {()=>setDarkMode(pre=>!pre)}>D&T</button>
+    
     </nav>
      );
 }
  
 export default Navbar;
+
